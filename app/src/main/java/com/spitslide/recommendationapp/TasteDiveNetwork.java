9cb000b9522afe4b61f7b0ab457b120d636487a7
digitapex <spitslide@gmail.com>
2018-11-10 10:27:50 +0100

    Add network interface

--- app/src/main/java/com/spitslide/recommendationapp/TasteDiveNetwork.java
+++ app/src/main/java/com/spitslide/recommendationapp/TasteDiveNetwork.java
@@ -1,10 +1,14 @@
 package com.spitslide.recommendationapp;
 
 
+import com.spitslide.recommendationapp.tastedive.Similar;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface TasteDiveNetwork {
 
-    @GET
-
+    @GET("api/similar")
+    Call<Similar> getReponse(@Query("k") String apiKey, @Query("q") String query);
 }
