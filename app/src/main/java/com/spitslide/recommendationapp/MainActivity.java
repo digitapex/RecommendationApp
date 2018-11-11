a4aede8c3f842462f184e6e29aa8b8308e2d7192
digitapex <spitslide@gmail.com>
2018-11-11 12:20:30 +0100

    Move the taste dive call to the second activity

--- app/src/main/java/com/spitslide/recommendationapp/MainActivity.java
+++ app/src/main/java/com/spitslide/recommendationapp/MainActivity.java
@@ -2,22 +2,7 @@ package com.spitslide.recommendationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
-import com.spitslide.recommendationapp.tastedive.Similar;
-
-import java.util.Collections;
-
-import okhttp3.CipherSuite;
-import okhttp3.ConnectionSpec;
-import okhttp3.OkHttpClient;
-import okhttp3.TlsVersion;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,50 +13,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSearch(View view) {
-        retrofitConnection();
 
     }
 
-    private void retrofitConnection() {
-        // this needed because there's a bug on pre-Lollipop for TLS
-        // https://stackoverflow.com/a/50640113/9702500
-        ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.COMPATIBLE_TLS)
-                .supportsTlsExtensions(true)
-                .tlsVersions(TlsVersion.TLS_1_2, TlsVersion.TLS_1_1, TlsVersion.TLS_1_0)
-                .cipherSuites(
-                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-                        CipherSuite.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
-                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_CBC_SHA,
-                        CipherSuite.TLS_DHE_DSS_WITH_AES_128_CBC_SHA,
-                        CipherSuite.TLS_DHE_RSA_WITH_AES_256_CBC_SHA)
-                .build();
-        OkHttpClient client = new OkHttpClient.Builder()
-                .connectionSpecs(Collections.singletonList(spec))
-                .build();
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://tastedive.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build();
-        TasteDiveNetwork tasteDiveNetwork = retrofit.create(TasteDiveNetwork.class);
-        Call<Similar> call = tasteDiveNetwork.getReponse(BuildConfig.TASTEDIVE_API_KEY, "pulp");
-        call.enqueue(new Callback<Similar>() {
-            @Override
-            public void onResponse(Call<Similar> call, Response<Similar> response) {
-                Log.d("MY", "reponse: " + response.body().getSimilar().getResults().get(3).getName());
-            }
 
-            @Override
-            public void onFailure(Call<Similar> call, Throwable t) {
-
-            }
-        });
-    }
 }
+
